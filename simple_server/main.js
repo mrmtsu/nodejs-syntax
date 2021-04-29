259e3ae57544db8a294edc63c1fa13aeffddd2c4
mrmtsu <81202muramatsu@gmail.com>
2021-04-30 06:52:22 +0900

    add: routing

---
+++ simple_server/main.js
@@ -0,0 +1,23 @@
+const port = 3000,
+    // httpとhttp-status-codeのモジュールをロードする
+    http = require("http"),
+    httpStatus = require("http-status-codes"),
+    // requestとresponseのパラメータを指定してサーバを作成
+    app = http.createServer((request, response) => {
+      // リクエストを受信しました
+      console.log("Received an incoming request!");
+      // クライアントに対するレスポンスを書く
+      response.writeHead(httpStatus.OK, {
+        "Content-Type": "text/html"
+      });
+
+      let responseMessage = "<h1>Hello, Universe!</h1>"
+      response.write(responseMessage);
+      response.end();
+      // レスポンスを送信しました
+      console.log(`Sent a response : ${responseMessage}`);
+      // アプリケーションサーバにポート3000を監視させる
+      app.listen(port);
+      // サーバが起動して、このポートを監視中
+      console.log(`The server has started and is listening on port number: ${port}`);
+    })
